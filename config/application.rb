302438d326d8275b7e93887ac0b56846104e7efe
lilawalker <lilawlker@gmail.com>
2020-04-28 12:52:23 +0100

    Add rspotify for oauth - currently returing 400 bad request

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,10 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require 'rspotify'
+
+RSpotify::authenticate('spotify_client_id', 'spotify_client_secret')
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
