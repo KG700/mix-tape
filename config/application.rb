45e8d719b105ff696cab7ed427d8b3868f66d82a
lilawalker <lilawlker@gmail.com>
2020-04-28 16:42:57 +0100

    Remove figaro and add foreman app

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 require 'rspotify'
 
-RSpotify::authenticate(ENV['spotify_client_id'], ENV['spotify_client_secret'])
+RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
