582cfaa7b2b82830a3ba3b3b390d1df1db5ebbbb
Sophia Bell <sophbell87@gmail.com>
2020-04-28 15:51:31 +0100

    not working

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 # require 'rspotify/oauth'
 
 # Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :spotify, 'spotify_client_id', 'spotify_client_secret',
+#   provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'],
 #   scope: 'user-top-read'
 # end
