9afed8dddde6577f37b82d8413b52c922a6a0179
Sophia Bell <sophbell87@gmail.com>
2020-04-28 12:47:51 +0100

    adding nav bar component

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Nav from "./Nav";
+
 
 export default () => (
+  <div><Nav/>
   <div>Welcome to Mix-tape</div>
+  </div> 
 )
