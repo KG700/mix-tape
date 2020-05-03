e44a53b6ce6d6cf3d96427bca4e174fcb024bb6a
Karla <karla.gardiner@gmail.com>
2020-05-03 12:11:55 +0100

    set up render json in Player controler to send player_id to react

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -2,13 +2,16 @@ class Api::V1::PlaylistsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-  
+    render json: session[:playlist_id]
   end
 
   def create
+    p current_user
     user = RSpotify::User.find(current_user.spotify_id)
     playlist_tracks_ids = params[:playlist]
-    playlist = user.create_playlist!('My Mixtape')
+    p playlist_tracks_ids
+    playlist = user.create_playlist!('My-Mixtape')
+    p playlist
     playlist_tracks = []
     playlist_tracks_ids.each do |track|
       playlist_tracks << RSpotify::Track.find(track)
