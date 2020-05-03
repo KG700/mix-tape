1cb31c0dcd89cd822d8004dff742ddbcc834885f
Karla <karla.gardiner@gmail.com>
2020-05-03 11:44:37 +0100

    Embeded a player into a Player component and connected to a playlist in spotify

---
+++ app/javascript/components/Player.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+class Player extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+
+render() {
+  let track_id = "4AMxwqRTwJMhDBwzlhc3XI"
+  let track_url ="https://open.spotify.com/embed/playlist/" + track_id
+
+  return(
+    <iframe
+      src={track_url}
+      width="300"
+      height="380"
+      frameBorder="0"
+      allowtransparency="true"
+      allow="encrypted-media">
+    </iframe>
+
+  )
+}
+
+}
+
+export default Player;
