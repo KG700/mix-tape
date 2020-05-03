27c50555da519048ea51827d099e0ef663faaaca
Sophia Bell <sophbell87@gmail.com>
2020-05-03 10:27:18 +0100

    rubocop

--- spec/Controllers/login_controller_spec.rb
+++ spec/Controllers/login_controller_spec.rb
@@ -4,12 +4,11 @@ require 'rails_helper'
 
 RSpec.describe LoginController, type: :controller do
 
-
- it "allows authenticated access" do
-  sign_in
-  get :index
-  
-  expect(response).to be_success
-end
+  it "allows authenticated access" do
+    sign_in
+    get :index
+   
+    expect(response).to be_success
+  end
 
 end
--- spec/Helpers/controller_helpers.rb
+++ spec/Helpers/controller_helpers.rb
@@ -1,7 +1,7 @@
 module ControllerHelpers
   def sign_in(user = double('user'))
     if user.nil?
-      allow(request.env['mix_tape_test']).to receive(:authenticate!).and_throw(:mix_tape_test, {:scope => :user})
+      allow(request.env['mix_tape_test']).to receive(:authenticate!).and_throw(:mix_tape_test, { :scope => :user })
       allow(controller).to receive(:user).and_return(nil)
     else
       allow(request.env['mix_tape_test']).to receive(:authenticate!).and_return(user)
