ffeff252ca63dedac87401790a24f0ea5ffc9a05
lilawalker <lilawlker@gmail.com>
2020-05-04 21:31:42 +0100

    Add track model tests

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,6 @@
 class Track < ApplicationRecord
   has_many :user_tracks, dependent: :destroy
   has_many :users, through: :user_tracks
+
+  validates :spotify_track_id, :track_name, :track_url, :artist_name, presence: true
 end
