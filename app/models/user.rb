3cf13e04b842089642a4ba282791110b29ebc5b8
lilawalker <lilawlker@gmail.com>
2020-05-04 21:21:10 +0100

    Add user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :user_tracks, dependent: :destroy
   has_many :tracks, through: :user_tracks, dependent: :destroy
+
+  validates :username, :auth_token, :spotify_id, :image_url, presence: true
 end
