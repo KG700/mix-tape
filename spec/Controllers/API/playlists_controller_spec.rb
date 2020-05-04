f7ef065c58b0bfb277d23222d5ca6e2903646eb4
Sophia Bell <sophbell87@gmail.com>
2020-05-04 21:45:17 +0100

    rubocop

--- spec/Controllers/API/playlists_controller_spec.rb
+++ spec/Controllers/API/playlists_controller_spec.rb
@@ -1,11 +1,10 @@
-
 # frozen_string_literal: true
 
 require 'rails_helper'
 
 RSpec.describe Api::V1::PlaylistsController, type: :controller do
   
-  let(:user) {double :user, playlist_id: "H40ldfk2l234lsdoi"}
+  let(:user) { double :user, playlist_id: "H40ldfk2l234lsdoi" }
 
   before(:each) do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
@@ -15,7 +14,7 @@ RSpec.describe Api::V1::PlaylistsController, type: :controller do
     it 'responds with 200' do
       get :index
       expect(response).to have_http_status(200)
-      expect(JSON.parse(response.body)).to eq({"player_id"=>"H40ldfk2l234lsdoi"})
+      expect(JSON.parse(response.body)).to eq({ "player_id" => "H40ldfk2l234lsdoi" })
     end
 
   end
@@ -34,7 +33,6 @@ RSpec.describe Api::V1::PlaylistsController, type: :controller do
   # it 'creates a playlist' do
    
   # end
-
 
   end
 
--- spec/Controllers/API/tracks_controller_spec.rb
+++ spec/Controllers/API/tracks_controller_spec.rb
@@ -1,51 +1,34 @@
-
 # frozen_string_literal: true
 
 require 'rails_helper'
 
 RSpec.describe Api::V1::TracksController, type: :controller do
 
-  # let(:user) { double :user }
-
-  # before(:each) do
-  #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-  # end
-
-
-  # let(:user) {double :user }
-  # let(:id) { '1' }
-  # let(:tracks) {double :tracks}
-
-  #   before do
-  #     user.stub(:id).and_return(id)
-  #     User.stub(:find).and_return(user)
-  #     user.stub(:tracks).and_return(Array)
-  #   end
-  
-  # describe 'GET /index ' do
-  #   it 'responds with 200' do
-  #     get :index, params: { user_id: user.id }
-  #     expect(response).to have_http_status(200)
-  #   end
-
-  # #   # it 'returns a json response' do
-  # #   #   expect(response.content_type).to eq("application/json")
-  # #   # end
-  # end
+  describe 'GET /index ' do
+    it 'responds with 200' do
+      expect(response).to have_http_status(200)
+    end
+  end
 
   # context "when there are no tracks in the database" do
-  #   let(:tracks) { Track.none }
 
+  #   let(:user) { double :user}
+  #   # let(:track) {double :track}
+  #   let(:track) { Track.none }
+    
   #   before do
-  #     allow(Track).to receive(:all).and_return(tracks)
+  #   #   allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+  #   #   allow(user).to receive(:user_id) { user.id }
+  #     allow(Track).to receive(:all).and_return(track)
 
-  #     get :index, params: { users: user.id }
+  #   #   get :index, params: {user_id: user.id} 
+
   #   end
 
-  #   specify { expect(response).to have_http_status(200) }
-  #   specify { expect(JSON.parse(response.body)).to eq([]) }
+  #   it 'should have an empty json response' do
+  #     expect(JSON.parse(response.body)).to eq(user.track)
+  #   end
+    
   # end
-  
-
 
 end
