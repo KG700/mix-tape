87d11e93796a957696739492f7c11ed3f79bd431
Sophia Bell <sophbell87@gmail.com>
2020-05-04 17:46:31 +0100

    some tests for api controllers

---
+++ spec/Controllers/API/currentuser_controller_spec.rb
@@ -0,0 +1,15 @@
+
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::CurrentuserController, type: :controller do
+
+  # describe 'GET /index ' do
+  #   it 'responds with 200' do
+  #     get :index
+  #     expect(response).to have_http_status(200)
+  #   end
+  # end
+
+end
