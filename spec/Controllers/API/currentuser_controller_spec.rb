3a88712d95d639e60e11893463433f1d756e3e6c
Sophia Bell <sophbell87@gmail.com>
2020-05-04 19:37:58 +0100

    currentusercontroller test

--- spec/Controllers/API/currentuser_controller_spec.rb
+++ spec/Controllers/API/currentuser_controller_spec.rb
@@ -5,11 +5,11 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::CurrentuserController, type: :controller do
 
-  # describe 'GET /index ' do
-  #   it 'responds with 200' do
-  #     get :index
-  #     expect(response).to have_http_status(200)
-  #   end
-  # end
+  describe 'GET /index ' do
+    it 'responds with 200' do
+      get :index
+      expect(response).to have_http_status(200)
+    end
+  end
 
 end
