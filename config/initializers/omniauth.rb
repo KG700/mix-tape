0dbd56e629b54632458f4b5a24a9e35c8a41b27d
Karla <karla.gardiner@gmail.com>
2020-05-06 12:20:07 +0100

    Updated spotify scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,5 +5,5 @@ Rails.application.config.to_prepare do
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'], scope: 'user-top-read playlist-modify-public playlist-modify-private'
+  provider :spotify, ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'], scope: 'user-top-read playlist-modify-public playlist-modify-private user-read-email'
 end
